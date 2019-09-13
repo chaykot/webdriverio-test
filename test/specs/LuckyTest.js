85f9be946d1277b50704dd502f5009e83ea635b2
chaykot <tatianachaiko1501@gmail.com>
2019-09-13 15:55:42 +0300

    update tests

--- test/specs/LuckyTest.js
+++ test/specs/LuckyTest.js
@@ -3,6 +3,6 @@ import MainPage from '../../pages/Main.page';
 describe('Google Lucky button', () => {
     it('should work correctly', () => {
         MainPage.open();
-        MainPage.getLucky('какой-то запрос');
+        MainPage.getLucky('какой-то запрос в google');
     });
 });
--- test/specs/SearchTest.js
+++ test/specs/SearchTest.js
@@ -5,7 +5,7 @@ var assert = require('chai').assert;
 describe('Google search', () => {
     it('should work correctly', () => {
         MainPage.open();
-        MainPage.search('какой-то запрос');
+        MainPage.search('какой-то запрос в google');
         assert.equal(ResultsPage.resultStats.isDisplayed(), false, "Results stats is not present");
     });
 });
